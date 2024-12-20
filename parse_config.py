7a778930332a9a88d88a6df3c1ccd25b309d6238
SUGAR-ADDICT <1792821909@qq.com>
2024-12-21 02:47:07 +0800

    设置读取文件

---
+++ parse_config.py
@@ -0,0 +1,22 @@
+import yaml
+
+
+def frange(start, end, step):
+    while start <= end:
+        yield round(start, 8)  # 避免浮点数累积误差
+        start += step
+
+
+def parse_yaml_config(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        config = yaml.safe_load(f)  # 使用 safe_load 来加载文件
+
+    # 处理配置中的 start, end, step 为列表的逻辑
+    for key in config['wave']:
+        if isinstance(config['wave'][key], dict) and 'start' in config['wave'][key]:
+            start = config['wave'][key]['start']
+            end = config['wave'][key]['end']
+            step = config['wave'][key].get('step', 1)  # 默认为1
+            config['wave'][key] = list(
+                frange(start, end, step))  # 使用 numpy 生成浮点范围
+    return config
