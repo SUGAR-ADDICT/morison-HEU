8c99c78032ea8cc37483f462fdde0b3154607464
SUGAR-ADDICT <1792821909@qq.com>
2024-12-25 23:37:59 +0800

    添加命令行参数

--- show_results.py
+++ show_results.py
@@ -2,10 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import re
-os.environ['TCL_LIBRARY'] = r'D:/Application/Python3.13.0/tcl/tcl8.6'
+import argparse  # 导入 argparse 模块
+
+os.environ["TCL_LIBRARY"] = r"D:/Application/Python3.13.0/tcl/tcl8.6"
 
 
-file_path = "results.txt"
+# 使用 argparse 获取命令行参数
+parser = argparse.ArgumentParser(
+    description="Plot forces over time from a specified data file."
+)
+parser.add_argument("fpath", type=str, help="Path to the data file (e.g., results.txt)")
+args = parser.parse_args()  # 解析命令行参数
+
+file_path = args.fpath  # 获取文件路径file_path = "results.txt"
 
 legend_lst = []
 with open(file_path, "r") as f:
@@ -14,18 +23,18 @@ with open(file_path, "r") as f:
     case_name_lst.pop(0)  # 删掉开头的#case name 关键字
 
 
-data = np.loadtxt(file_path, comments='#')
+data = np.loadtxt(file_path, comments="#")
 time = data[:, 0]
 plt.figure(figsize=(10, 6))
 
 for i, case_name in enumerate(case_name_lst):
-    force = data[:, i+1]
+    force = data[:, i + 1]
     plt.plot(time, force)
 
 
 # 设置图表标题和标签
-plt.title('Forces Over Time')
-plt.xlabel('Time (s)')
-plt.ylabel('Force (N)')
+plt.title("Forces Over Time")
+plt.xlabel("Time (s)")
+plt.ylabel("Force (N)")
 plt.legend(case_name_lst)
 plt.show()
